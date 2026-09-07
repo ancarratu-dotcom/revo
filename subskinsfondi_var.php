4377cb50f1f8b7bf3cf5f04091660ff087bb2673
ancarratu-dotcom <an.carratu@gmail.com>
2026-09-07 15:41:33 +0200

    Add files via upload

---
+++ subskinsfondi_var.php
@@ -0,0 +1,9 @@
+<?php
+$col_chiaro1="#999999"; //Colore scuro per il box di tipo 1
+$col_chiaro2="#ffffff"; //Colore chiaro per il box di tipo 1
+$col_scuro1="#92b92e";  //Colore scuro per il box di tipo 2
+$col_scuro2="#b9da50";  //Colore chiaro per il box di tipo 2
+
+$col_barre_capoclassifica1 = "#FFFFFF"; //Colore per le barre che compaiono al fianco del capoclassifica in home page
+$col_barre_capoclassifica2 = "#FFFFFF"; //Colore per le barre che compaiono al fianco del capoclassifica in home page
+?>
